eb04aebb299af6a0f0d81f9c7e6dcf1730113642
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-01-17 19:14:33 +0530

    Update 43_multiply_strings.java

--- google-prep/arrays-strings/43_multiply_strings.java
+++ google-prep/arrays-strings/43_multiply_strings.java
@@ -133,3 +133,86 @@ class Solution {
         return checkZero(adds[0]);
     }
 }
+
+
+/**
+Better solution:
+https://leetcode.com/explore/interview/card/google/59/array-and-strings/3051/discuss/17605/Easiest-JAVA-Solution-with-Graph-Explanation
+
+Start from right to left, perform multiplication on every pair of digits, and add them together. Let's draw the process! 
+https://drscdn.500px.org/photo/130178585/m%3D2048/300d71f784f679d5e70fadda8ad7d68f
+
+From the following draft, we can immediately conclude:
+
+ `num1[i] * num2[j]` will be placed at indices `[i + j`, `i + j + 1]` 
+
+*/
+
+public String multiply(String num1, String num2) {
+    int m = num1.length(), n = num2.length();
+    int[] pos = new int[m + n];
+   
+    for(int i = m - 1; i >= 0; i--) {
+        for(int j = n - 1; j >= 0; j--) {
+            int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0'); 
+            int p1 = i + j, p2 = i + j + 1;
+            int sum = mul + pos[p2];
+
+            pos[p1] += sum / 10;
+            pos[p2] = (sum) % 10;
+        }
+    }  
+    
+    StringBuilder sb = new StringBuilder();
+    for(int p : pos) if(!(sb.length() == 0 && p == 0)) sb.append(p);
+    return sb.length() == 0 ? "0" : sb.toString();
+}
+
+// Similar to above but easier to understand
+
+public class Solution {
+    public String multiply(String num1, String num2) {
+        int n1 = num1.length(), n2 = num2.length();
+        
+        int[] products = new int[n1 + n2];
+        
+        for (int i = n1 - 1; i >= 0; i--) {
+            for (int j = n2 - 1; j >= 0; j--) {
+                int d1 = num1.charAt(i) - '0';
+                int d2 = num2.charAt(j) - '0';
+                products[i + j + 1] += d1 * d2;
+            }
+        }
+        
+        int carry = 0;
+        
+        for (int i = products.length - 1; i >= 0; i--) {
+            int tmp = (products[i] + carry) % 10;
+            carry = (products[i] + carry) / 10;
+            products[i] = tmp;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int num : products) sb.append(num);
+        
+        while (sb.length() != 0 && sb.charAt(0) == '0') sb.deleteCharAt(0);
+        
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
+
+/**
+If we break it into steps, it will have the following steps. 
+1. compute products from each pair of digits from num1 and num2. 
+2. carry each element over. 
+3. output the solution.
+
+Things to note:
+
+    The product of two numbers cannot exceed the sum of the two lengths. (e.g. 99 * 99 cannot be five digit)
+
+int d1 = num1.charAt(i) - '0';
+int d2 = num2.charAt(j) - '0';
+*/
+products[i + j + 1] += d1 * d2;
