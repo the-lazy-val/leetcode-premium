960cba350523c4f0874023ac63454953ae1780ab
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-01-17 20:19:37 +0530

    Update 48_rotate_matrix.java

--- google-prep/arrays-strings/48_rotate_matrix.java
+++ google-prep/arrays-strings/48_rotate_matrix.java
@@ -56,3 +56,31 @@ class Solution {
         }
     }
 }
+
+
+// LC solution without extra space of visited matrix
+
+class Solution {
+  public void rotate(int[][] matrix) {
+    int n = matrix.length;
+    for (int i = 0; i < n / 2 + n % 2; i++) {
+      for (int j = 0; j < n / 2; j++) {
+        int[] tmp = new int[4];
+        int row = i;
+        int col = j;
+        for (int k = 0; k < 4; k++) {
+          tmp[k] = matrix[row][col];
+          int x = row;
+          row = col;
+          col = n - 1 - x;
+        }
+        for (int k = 0; k < 4; k++) {
+          matrix[row][col] = tmp[(k + 3) % 4];
+          int x = row;
+          row = col;
+          col = n - 1 - x;
+        }
+      }
+    }
+  }
+}
