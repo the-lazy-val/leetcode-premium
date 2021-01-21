59d7a3cd38a0fa82ff248c9a879cac589b805483
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-01-21 21:26:57 +0530

    Update 849_Maximize_Distance_to_Closest_Person.java

--- google-prep/arrays-strings/849_Maximize_Distance_to_Closest_Person.java
+++ google-prep/arrays-strings/849_Maximize_Distance_to_Closest_Person.java
@@ -60,3 +60,98 @@ class Solution {
         return max;
     }
 }
+
+/**
+LC solution: 
+Approach #1: Next Array
+
+Let left[i] be the distance from seat i to the closest person sitting to the left of i. Similarly, let right[i] be the distance to the closest person sitting to the right of i. This is motivated by the idea that the closest person in seat i sits a distance min(left[i], right[i]) away.
+
+Algorithm
+
+To construct left[i], notice it is either left[i-1] + 1 if the seat is empty, or 0 if it is full. right[i] is constructed in a similar way.
+
+
+
+    Time Complexity: O(N), where N is the length of seats.
+
+    Space Complexity: O(N), the space used by left and right.
+*/
+class Solution {
+    public int maxDistToClosest(int[] seats) {
+        int N = seats.length;
+        int[] left = new int[N], right = new int[N];
+        Arrays.fill(left, N);
+        Arrays.fill(right, N);
+
+        for (int i = 0; i < N; ++i) {
+            if (seats[i] == 1) left[i] = 0;
+            else if (i > 0) left[i] = left[i-1] + 1;
+        }
+
+        for (int i = N-1; i >= 0; --i) {
+            if (seats[i] == 1) right[i] = 0;
+            else if (i < N-1) right[i] = right[i+1] + 1;
+        }
+
+        int ans = 0;
+        for (int i = 0; i < N; ++i)
+            if (seats[i] == 0)
+                ans = Math.max(ans, Math.min(left[i], right[i]));
+        return ans;
+    }
+}
+
+
+/**
+LC solution
+Approach #2: Two Pointer 
+
+Intuition
+
+As we iterate through seats, we'll update the closest person sitting to our left, and closest person sitting to our right.
+
+Algorithm
+
+Keep track of prev, the filled seat at or to the left of i, and future, the filled seat at or to the right of i.
+
+Then at seat i, the closest person is min(i - prev, future - i), with one exception. i - prev should be considered infinite if there is no person to the left of seat i, and similarly future - i is infinite if there is no one to the right of seat i.
+
+
+
+
+Time Complexity: O(N), where NNN is the length of seats.
+
+Space Complexity: O(1)
+*/
+
+class Solution {
+    public int maxDistToClosest(int[] seats) {
+        int N = seats.length;
+        int prev = -1, future = 0;
+        int ans = 0;
+
+        for (int i = 0; i < N; ++i) {
+            if (seats[i] == 1) {
+                prev = i;
+            } else {
+                while (future < N && seats[future] == 0 || future < i)
+                    future++;
+
+                int left = prev == -1 ? N : i - prev;
+                int right = future == N ? N : future - i;
+                ans = Math.max(ans, Math.min(left, right));
+            }
+        }
+
+        return ans;
+    }
+}
+
+
+/**
+LC solution
+Approach #3: Group by Zero
+
+My approach
+*/
