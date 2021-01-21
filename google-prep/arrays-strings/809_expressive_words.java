0d8654d6c450a2ad99c132ac971c6f7622bc4907
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-01-21 08:39:55 +0530

    Update 809_expressive_words.java

--- google-prep/arrays-strings/809_expressive_words.java
+++ google-prep/arrays-strings/809_expressive_words.java
@@ -150,3 +150,59 @@ class RLE {
         key = sb.toString();
     }
 }
+
+/**
+Two - pointer approach
+
+We have two pointers, use i to scan S, and use j to scan each word in words.
+Firstly, for S and word, we can calculate the length of the susbtrings which contains the repeated letters of the letter currently pointed by the two pointers, and get len1 and len2.
+
+The two letters currently pointed by the two pointers must be equal, otherwise the word is not stretchy, we return false. Then, if we find that len1 is smaller than 3, it means the letter cannot be extended, so len1 must equals to len2, otherwise this word is not stretchy. In the other case, if len1 equals to or larger than 3, we must have len1 equals to or larger than len2, otherwise there are not enough letters in S to match the letters in word.
+
+Finally, if the word is stretchy, we need to guarantee that both of the two pointers has scanned the whole string.
+*/
+
+class Solution {
+    public int expressiveWords(String S, String[] words) {
+        if (S == null || words == null) {
+            return 0;
+        }
+        int count = 0;
+        for (String word : words) {
+            if (stretchy(S, word)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    public boolean stretchy(String S, String word) {
+        if (word == null) {
+            return false;
+        }
+        int i = 0;
+        int j = 0;
+        while (i < S.length() && j < word.length()) {
+            if (S.charAt(i) == word.charAt(j)) {
+                int len1 = getRepeatedLength(S, i);
+                int len2 = getRepeatedLength(word, j);
+                if (len1 < 3 && len1 != len2 || len1 >= 3 && len1 < len2) {
+                    return false;
+                }
+                i += len1;
+                j += len2;
+            } else {
+                return false;
+            }
+        }
+        return i == S.length() && j == word.length();
+    }
+    
+    public int getRepeatedLength(String str, int i) {
+        int j = i;
+        while (j < str.length() && str.charAt(j) == str.charAt(i)) {
+            j++;
+        }
+        return j - i;
+    }
+}
