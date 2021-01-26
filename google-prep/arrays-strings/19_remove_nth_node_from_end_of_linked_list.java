ecc531675508c379e4050cac26dcb6509b6cf007
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-01-26 14:05:02 +0530

    Update 19_remove_nth_node_from_end_of_linked_list.java

--- google-prep/arrays-strings/19_remove_nth_node_from_end_of_linked_list.java
+++ google-prep/arrays-strings/19_remove_nth_node_from_end_of_linked_list.java
@@ -29,3 +29,24 @@ class Solution {
         return head;
     }
 }
+
+
+/**
+Alternative implementation of same thing but 0 ms
+*/
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode left = dummy, right = dummy;
+        for(int i = 0; i <= n; i++)
+            right = right.next;
+        while(right != null) {
+            left = left.next;
+            right = right.next;
+        }
+        left.next = left.next.next;
+        
+        return dummy.next;
+    }
+}
