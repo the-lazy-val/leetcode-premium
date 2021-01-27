8e9598d9f7dd4522760f6508965316b8f4a42a9f
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-01-27 23:47:31 +0530

    Update 200_number_of_islands.java

--- google-prep/arrays-strings/200_number_of_islands.java
+++ google-prep/arrays-strings/200_number_of_islands.java
@@ -8,6 +8,8 @@ beats 100%
 Time complexity : O(M×N) where M is the number of rows and N is the number of columns.
 
 Space complexity : worst case O(M×N) in case that the grid map is filled with lands where DFS goes by M×N deep
+
+An improvement can be, instead of maintaining a visited 2d array, just mark the element as '0'
 */
 
 class Solution {
