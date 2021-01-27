283ceb927dcc94df8e7c9e8c67b009976b1ae21c
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-01-27 23:43:41 +0530

    Update 200_number_of_islands.java

--- google-prep/arrays-strings/200_number_of_islands.java
+++ google-prep/arrays-strings/200_number_of_islands.java
@@ -2,6 +2,12 @@
 My solution using DFS
 1 ms
 beats 100%
+
+
+
+Time complexity : O(M×N) where M is the number of rows and N is the number of columns.
+
+Space complexity : worst case O(M×N) in case that the grid map is filled with lands where DFS goes by M×N deep
 */
 
 class Solution {
