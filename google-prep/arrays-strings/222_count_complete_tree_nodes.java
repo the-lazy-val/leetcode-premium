e89b95331ffe68d5e3bec755c00a9b1981eb7ce9
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-01-28 23:43:58 +0530

    Update 222_count_complete_tree_nodes.java

--- google-prep/arrays-strings/222_count_complete_tree_nodes.java
+++ google-prep/arrays-strings/222_count_complete_tree_nodes.java
@@ -31,4 +31,62 @@ Since in Binary Complete tree, at last level, all nodes are towards left
 We can Binary Search to find the how many nodes are there in the last level
 
 https://docs.google.com/document/d/1csfxOIOuV6xkuFJ8cT3SQUasO7drOgqyIRwtXkbB9mU/edit?usp=sharing
+
+Time complexity = O(d^2) = O(log^2 N)
 */
+
+class Solution {
+    
+    public int getDepth(TreeNode root){
+        int depth = 0;
+        while(root.left!=null){
+            root = root.left;
+            depth++;
+        }
+        
+        return depth;
+    }
+    
+    public boolean exists(TreeNode node, int idx, int d){ 
+        int left = 0;
+        int right = (int)Math.pow(2, d) - 1;
+        int pivot;
+        
+        //doing binary search, here for loop is limiting the iteration to depth so we can't go further than last level
+        
+        //observe node.left & node.right, with each iteration we are going down, 
+        //with respect to what the position of current node is in respect to the PIVOT on last level
+        
+        for(int i = 0; i < d; ++i) {
+            pivot = left + (right - left) / 2;
+            if (idx <= pivot) {
+                node = node.left;
+                right = pivot;
+            }
+            else {
+                node = node.right;
+                left = pivot + 1;
+            }
+        }
+        return node != null;
+    }
+    
+    public int countNodes(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        int d = getDepth(root);
+        
+        if(d==0) return 1;
+        
+        int left = 0;
+        int right = (int)Math.pow(2, d) - 1;
+        while(left<=right){
+            int pivot = left + (right-left)/2;
+            if(exists(root, pivot, d)) left=pivot+1;
+            else right=pivot-1;
+        }
+        
+        return (int)Math.pow(2,d)-1+left;
+    }
+}
