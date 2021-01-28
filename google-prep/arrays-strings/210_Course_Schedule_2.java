4c54a3d69c8e5a9ba14c8a75ca8568f2807b9936
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-01-28 22:41:59 +0530

    Update 210_Course_Schedule_2.java

--- google-prep/arrays-strings/210_Course_Schedule_2.java
+++ google-prep/arrays-strings/210_Course_Schedule_2.java
@@ -76,3 +76,67 @@ class Solution {
         
     }
 }
+
+/**
+Cycle detection as part of DFS
+
+If element is already visited, but is not part of output stack, that means there is a cycle
+https://www.youtube.com/watch?v=Akt3glAwyfY
+*/
+
+class Solution {
+    
+    Stack<Integer> st;
+    boolean[] visit;
+    boolean flag;
+    
+    public void dfs(ArrayList<Integer>[] adj, boolean[] visit, int node, Stack<Integer> st){
+        visit[node] = true;
+        for(int i : adj[node]){
+            if(!visit[i]) {
+                dfs(adj, visit, i, st);
+            }else{
+                if(!st.contains(i)){ //course is not added to output(stack) but is visited, so its a cycle
+                    flag = true;
+                    break;
+                }
+            }
+        }
+        st.push(node);
+    }
+    
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        flag = false;
+        ArrayList<Integer>[] adj = new ArrayList[numCourses];
+        
+        for(int i=0; i<adj.length; i++){
+            adj[i] = new ArrayList<Integer>();
+        }
+        
+        for(int[] e : prerequisites){
+            adj[e[1]].add(e[0]);
+        }
+        
+        
+        st = new Stack<>();
+        visit = new boolean[numCourses];
+        
+        for(int i=0; i<numCourses; i++){
+            if(!visit[i]){
+                dfs(adj, visit, i, st);
+                if(flag){
+                    break;
+                }
+            }
+        }
+        
+        if(flag){
+            return new int[0];
+        }else{
+            List<Integer> li = new ArrayList(st);
+            Collections.reverse(li);
+            int[] res = li.stream().mapToInt(Integer::intValue).toArray();
+            return res;
+        }
+    }
+}
