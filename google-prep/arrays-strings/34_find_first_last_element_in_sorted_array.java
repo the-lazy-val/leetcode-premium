72120a86b42ec1cb5d3b35340110eedd67c2b3f6
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-01-31 21:43:52 +0530

    Update 34_find_first_last_element_in_sorted_array.java

--- google-prep/arrays-strings/34_find_first_last_element_in_sorted_array.java
+++ google-prep/arrays-strings/34_find_first_last_element_in_sorted_array.java
@@ -38,3 +38,48 @@ class Solution {
 /**
 second approach: binary search once, then iterate on both side
 */
+class Solution {
+    int min = Integer.MAX_VALUE;
+    int max = Integer.MIN_VALUE;
+    
+    public int find(int[] nums, int low, int high, int target){
+        if(low <= high){
+            
+        int mid = (low+high)/2;
+        int midValue = nums[mid];
+        
+        if(midValue < target){
+            return find(nums, mid+1, high, target);
+        }else if(midValue > target){
+            return find(nums, low, mid-1, target);
+        }else{
+            min = Math.min(min, mid);
+            max = Math.max(max, mid);
+            return mid;
+        }
+        }
+        return -1;
+    }
+    
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = new int[]{-1, -1};
+        
+        int firstMid = find(nums, 0, nums.length-1, target);
+        if(firstMid == -1){
+            return ans;
+        }
+        
+        //After finding the first position of target
+        //iteratively update min & max value, towards left & right
+        
+        while(min > 0 && nums[min-1] == target){
+            min--;
+        }
+        
+        while(max < nums.length-1 && nums[max+1] == target){
+            max++;
+        }
+        
+        return new int[]{min, max};
+    }
+}
