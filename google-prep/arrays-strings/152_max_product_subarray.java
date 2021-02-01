871d82b441704eb950ad168d086b16d7d4cc9d97
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-02-01 23:22:14 +0530

    Update 152_max_product_subarray.java

--- google-prep/arrays-strings/152_max_product_subarray.java
+++ google-prep/arrays-strings/152_max_product_subarray.java
@@ -2,6 +2,8 @@
 My solution
 186/187 test case passed
 
+Time: O(N), but space is O(N^2) because of matrix
+
 on very big array, memory limit exceeds
 
 Solved using the dp[][] matrix method, used in longest palindrome substring
@@ -29,6 +31,40 @@ class Solution {
                 max = Math.max(max, dp[i][i+inc]);
             }
             inc++;
+        }
+        
+        return max;
+    }
+}
+
+
+/**
+LC solution
+
+Time: O(N)
+Space: O(1)
+*/
+
+class Solution {
+    
+    public int maxProduct(int[] nums) {
+        int len = nums.length;
+        if(len==0) return 0;
+        
+        int maxSoFar = nums[0];
+        int minSoFar = nums[0];
+            
+        int max = maxSoFar;
+        
+        for(int i=1; i<nums.length; i++){
+            int curr = nums[i];
+            
+            int tempMax = Math.max(curr, Math.max(maxSoFar*curr, minSoFar*curr));
+            minSoFar = Math.min(curr, Math.min(maxSoFar*curr, minSoFar*curr));
+            
+            maxSoFar = tempMax;
+            
+            max = Math.max(max, maxSoFar);
         }
         
         return max;
