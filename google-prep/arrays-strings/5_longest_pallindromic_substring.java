d98e8a99574645bfa17c21a637c57ec46df9d043
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-02-01 17:01:45 +0530

    Update 5_longest_pallindromic_substring.java

--- google-prep/arrays-strings/5_longest_pallindromic_substring.java
+++ google-prep/arrays-strings/5_longest_pallindromic_substring.java
@@ -84,3 +84,23 @@ private void extendPalindrome(String s, int j, int k) {
 		maxLen = k - j - 1;
 	}
 }}
+
+/**
+Another variation of above solution
+*/
+    public String longestPalindrome(String s) {
+        String max = "";
+        for (int i = 0; i < s.length(); i++) {
+            String s1 = extend(s, i, i), s2 = extend(s, i, i + 1);
+            if (s1.length() > max.length()) max = s1;
+            if (s2.length() > max.length()) max = s2;
+        }
+        return max;
+    }
+    
+    private String extend(String s, int i, int j) {
+        for (; 0 <= i && j < s.length(); i--, j++) {
+            if (s.charAt(i) != s.charAt(j)) break;
+        }
+        return s.substring(i + 1, j);
+    }
