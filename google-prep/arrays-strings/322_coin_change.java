05808efcac000b14febc8667e8ffd25703ffb63b
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-02-02 16:38:58 +0530

    Update 322_coin_change.java

--- google-prep/arrays-strings/322_coin_change.java
+++ google-prep/arrays-strings/322_coin_change.java
@@ -78,3 +78,78 @@ class Solution {
         return find(coins, amount);
     }
 }
+
+
+/**
+Minor optimization on above code:
+
+24 ms
+
+beats 33%
+*/
+class Solution {
+    int[] dp;
+    
+    public int find(int[] coins, int amount){
+        if(amount < 0){
+            return -1;
+        }else if(amount==0){
+            return 0;
+        }else if(dp[amount] != 0){
+            return dp[amount];
+        }else{
+            if(amount < coins[0]){
+                dp[amount] = -1;
+                return -1;
+            }else{
+                int min = Integer.MAX_VALUE;
+                for(int c : coins){
+                    
+                    int temp = find(coins, amount-c);
+                    
+                    if(temp != -1){
+                        min = Math.min(min, 1 + temp);
+                    }    
+                }
+                if(min == Integer.MAX_VALUE){
+                    dp[amount] = -1;
+                    return -1;
+                }
+                dp[amount] = min;
+                return min;
+            }
+            
+        }
+        
+    }
+    
+    public int coinChange(int[] coins, int amount) {
+        
+        if(amount==0) return 0;
+        
+        Arrays.sort(coins);
+        
+        if(amount < coins[0]){
+            return -1;
+        }
+        
+        int len = 0;
+        for(int c : coins){
+            if(c <= amount) {
+                len++;
+            }else{
+                break;
+            }
+        }
+        
+        coins = Arrays.copyOfRange(coins, 0, len);
+        
+        dp = new int[Math.max(len, amount)+1];
+        
+        for(int c : coins){
+            dp[c] = 1;
+        }
+        
+        return find(coins, amount);
+    }
+}
