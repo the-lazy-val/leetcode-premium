92ea688c604ee0183fb84a37505fcc9dd65eb8af
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-02-05 00:07:32 +0530

    Update 1376_time_needed_to_inform_all_employees.java

--- top50-google/1376_time_needed_to_inform_all_employees.java
+++ top50-google/1376_time_needed_to_inform_all_employees.java
@@ -30,3 +30,30 @@ class Solution {
         return max;
     }
 }
+
+
+/**
+DFS + memoization
+
+need to understand
+
+https://leetcode.com/problems/time-needed-to-inform-all-employees/discuss/589738/Java-DFS-%2B-Memoization-(w-Explanation-and-Complexity-Analysis)
+
+*/
+class Solution {
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, dfs(i, manager, informTime));
+        }
+        return max;
+    }
+    
+    private int dfs(int id, int[] manager, int[] informTime) {
+        if (manager[id] != -1) {
+            informTime[id] += dfs(manager[id], manager, informTime);
+            manager[id] = -1;
+        }
+        return informTime[id];
+    }
+}
