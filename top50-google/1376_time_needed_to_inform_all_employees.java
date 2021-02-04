c48a966b0f35729d240dfb7c2844fffd67ad07ea
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-02-04 23:51:48 +0530

    Create 1376_time_needed_to_inform_all_employees.java

---
+++ top50-google/1376_time_needed_to_inform_all_employees.java
@@ -0,0 +1,32 @@
+/**
+My solution: 107 ms
+beats 65 %
+
+Using DFS and maintain max
+*/
+
+class Solution {
+    
+    HashMap<Integer, ArrayList<Integer>> hm = new HashMap();
+    int max = 0;
+    
+    public void dfs(int currId, int currTime, int[] informTime){
+        max = Math.max(max, currTime);
+        if(hm.containsKey(currId)){
+            for(int next : hm.get(currId)){
+                dfs(next, currTime+informTime[next], informTime);
+            }
+        }
+    }
+    
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        for(int i=0; i<manager.length; i++){
+            if(manager[i]!=-1){
+                hm.computeIfAbsent(manager[i], x -> new ArrayList()).add(i);
+            }
+        }
+        
+        dfs(headID, informTime[headID], informTime);
+        return max;
+    }
+}
