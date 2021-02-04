8a1bca6a541bbd71b21d74a2b2605a5741c550eb
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-02-04 21:23:56 +0530

    Update 1277_count_square_submatrices.java

--- top50-google/1277_count_square_submatrices.java
+++ top50-google/1277_count_square_submatrices.java
@@ -43,3 +43,33 @@ class Solution {
         return total;
     }
 }
+
+/**
+Better DP solution
+*/
+
+// You can also avoid additional for loops using dp[n + 1][m + 1] size, here is my example:
+
+class Solution {
+    public int countSquares(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        
+        int[][] dp = new int[n + 1][m + 1];
+        
+        int res = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                
+                if (matrix[i - 1][j - 1] == 1) {
+                    
+                    dp[i][j] = 1 + Math.min( Math.min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j - 1]);
+                    
+                    res += dp[i][j];
+                }
+                
+            }
+        }
+        return res;
+    }
+}
