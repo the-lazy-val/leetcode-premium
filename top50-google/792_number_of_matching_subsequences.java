801c59827e8454a9626e978c70a9ce4a53961ca8
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-02-06 10:46:53 +0530

    Create 792_number_of_matching_subsequences.java

---
+++ top50-google/792_number_of_matching_subsequences.java
@@ -0,0 +1,32 @@
+/**
+My solution
+
+42 / 49 test cases passed
+Time limit exceeds on case like:
+S: wwwwwwwwwww.....q
+words: [wq, wq, wq]
+*/
+
+class Solution {
+    
+    public int numMatchingSubseq(String S, String[] words) {
+        int s = 0;
+        while(s < S.length()){
+            char curr = S.charAt(s);
+            for(int i=0; i<words.length; i++){
+                String w = words[i];
+                if(w.length() > 0 && curr == w.charAt(0)){
+                    words[i] = w.substring(1);
+                }
+            }
+            s++;
+        }
+        
+        int output = 0;
+        for(int i=0; i<words.length; i++){
+            if(words[i].length() == 0) output++;
+        }
+        
+        return output;
+    }
+}
