91165db4e27ec80810eafdcb81023ec96d396cf9
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-02-07 18:31:04 +0530

    Create 42_trapping_rain_water.java

---
+++ google-frequency/arrays/42_trapping_rain_water.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    public int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+
+        int result = 0;
+        
+        // leftMax represents the highest bar from left
+        int leftMax = Integer.MIN_VALUE;
+        
+        // rightMax represents the highest bar from right
+        int rightMax = Integer.MIN_VALUE;
+
+        // use two pointers to scan the entire array until they meet with each other
+        // Key points: any bars in the middle of leftMax bar and rightMax bar will not influence
+        // how much water can current position trap
+        for (int left = 0, right = height.length - 1; left <= right;) {
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+            
+            //how much can current position trap depends on the shorter bar
+            if (leftMax < rightMax) {
+                //DO NOT FORGET to subtract bar height of current position
+                result += leftMax - height[left]; //since water can only be at top of that bar
+                left++;
+            }
+            else {
+                result += rightMax - height[right];
+                right--;
+            }
+        }
+        return result;
+    }
+}
