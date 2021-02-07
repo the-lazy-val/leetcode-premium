1dab36657000710bb29b94d3036b01d8a256620f
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-02-07 20:46:45 +0530

    Update 289_game_of_life.java

--- google-frequency/arrays/289_game_of_life.java
+++ google-frequency/arrays/289_game_of_life.java
@@ -55,3 +55,76 @@ class Solution {
         }
     }
 }
+
+
+/**
+For Infinite Board
+
+https://leetcode.com/problems/game-of-life/discuss/73217/Infinite-board-solution/76083
+*/
+
+    private Set<Coord> gameOfLife(Set<Coord> live) {
+        Map<Coord,Integer> neighbours = new HashMap<>();
+        for (Coord cell : live) {
+            for (int i = cell.i-1; i<cell.i+2; i++) {
+                for (int j = cell.j-1; j<cell.j+2; j++) {
+                    if (i==cell.i && j==cell.j) continue;
+                    Coord c = new Coord(i,j);
+                    if (neighbours.containsKey(c)) {
+                        neighbours.put(c, neighbours.get(c) + 1);
+                    } else {
+                        neighbours.put(c, 1);
+                    }
+                }
+            }
+        }
+        Set<Coord> newLive = new HashSet<>();
+        for (Map.Entry<Coord,Integer> cell : neighbours.entrySet())  {
+            if (cell.getValue() == 3 || cell.getValue() == 2 && live.contains(cell.getKey())) {
+                newLive.add(cell.getKey());
+            }
+        }
+        return newLive;
+    }
+
+//where Coord is:
+
+    private static class Coord {
+        int i;
+        int j;
+        private Coord(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+        public boolean equals(Object o) {
+            return o instanceof Coord && ((Coord)o).i == i && ((Coord)o).j == j;
+        }
+        public int hashCode() {
+            int hashCode = 1;
+            hashCode = 31 * hashCode + i;
+            hashCode = 31 * hashCode + j;
+            return hashCode;
+        }
+    }
+
+//and the wrapper:
+
+    public void gameOfLife(int[][] board) {
+        Set<Coord> live = new HashSet<>();
+        int m = board.length;
+        int n = board[0].length;
+        for (int i = 0; i<m; i++) {
+            for (int j = 0; j<n; j++) {
+                if (board[i][j] == 1) {
+                    live.add(new Coord(i,j));
+                }
+            }
+        };
+        live = gameOfLife(live);
+        for (int i = 0; i<m; i++) {
+            for (int j = 0; j<n; j++) {
+                board[i][j] = live.contains(new Coord(i,j))?1:0;
+            }
+        };
+        
+    }
