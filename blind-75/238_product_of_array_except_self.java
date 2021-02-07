e27e5bd48f573975488bf821e7216b8657840917
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-02-07 12:09:49 +0530

    Create 238_product_of_array_except_self.java

---
+++ blind-75/238_product_of_array_except_self.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        
+        int len = nums.length;
+        
+        int[] prefix = new int[len];
+        int[] suffix = new int[len];
+        
+        prefix[0] = 1;
+        
+        for(int i=1; i<len; i++){
+            prefix[i] = prefix[i-1]*nums[i-1];
+        }
+        
+        suffix[len-1] = 1;
+        
+        for(int i=len-2;  i>=0; i--){
+            suffix[i] = suffix[i+1]*nums[i+1];
+        }
+        
+        int[] output = new int[len];
+        for(int i=0; i<len; i++){
+            output[i] = prefix[i]*suffix[i];
+        }
+        
+        return output;
+    }
+}
