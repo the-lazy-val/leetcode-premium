ebd74988bac381e1316f881cdc5ed99fcabf2d65
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-02-08 15:19:07 +0530

    Update 128_longest_consequtive_sequence.java

--- google-frequency/arrays/128_longest_consequtive_sequence.java
+++ google-frequency/arrays/128_longest_consequtive_sequence.java
@@ -1,4 +1,42 @@
 /**
+O(N) solution
+
+instead of traversing over elements, we store all elements in HashSet
+
+And then, we take one element, and check if elem+1 is in HashSet & update count & max
+
+Before preocessing an element, we check if (elem-1) is in HashSet, because if it is, this means this element is already processed when calculating for some previous element
+*/
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        
+        int max= 0;
+        
+        HashSet<Integer> hs = new HashSet<>();
+        for(int n : nums){
+            hs.add(n);
+        }
+        
+        for(int i : nums){
+            if(hs.contains(i-1)){ //because if i-1 was in the original list, then it should have been already processed for longest sequence calculation
+                continue;
+            }else{
+                int currCount = 1;
+                int curr=i;
+                while(hs.contains(curr+1)){
+                    currCount++;
+                    curr++;
+                }
+                max = Math.max(max, currCount);
+            }
+        }
+        
+        return max;
+    }
+}
+
+/**
 My solution: O(nlogn)
 */
 
