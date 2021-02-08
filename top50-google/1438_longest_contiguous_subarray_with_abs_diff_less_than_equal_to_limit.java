9d5b8a1f2da1c1ed968badf9fa32747a8bbfeb3c
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-02-08 10:27:02 +0530

    Update 1438_longest_contiguous_subarray_with_abs_diff_less_than_equal_to_limit.java

--- top50-google/1438_longest_contiguous_subarray_with_abs_diff_less_than_equal_to_limit.java
+++ top50-google/1438_longest_contiguous_subarray_with_abs_diff_less_than_equal_to_limit.java
@@ -1,20 +1,31 @@
 class Solution {
     public int longestSubarray(int[] nums, int limit) {
         int left=0;
+        //We are using a TreeSet because for any range that we consider, 
+        //the difference b/w min & max element needs to be <= limit
         TreeSet<Integer> ts = new TreeSet<>((a,b) -> nums[a]==nums[b] ? a-b : nums[a]-nums[b]);
-        ts.add(0);
+        
+        ts.add(left); //adding first element
         
         int max=1;
-        
+
         for(int right=1; right<nums.length; right++){
-            ts.add(right);
+            
+            ts.add(right); //adding the next index
+            
+            //Now check, if adding this element, still maintains the diff b/w min & max element to be <= limit
+            //Since TreeSet maintains order, we can simply use:
+            //first() -> to get min
+            //last() -> to  get max
             
             while(nums[ts.last()] - nums[ts.first()] > limit){
-                ts.remove(left);
+                ts.remove(left);  //contract from left until diff b/w min & max <= limit
                 left++;
             }
+            
             max = Math.max(max, right-left+1);
         }
+        
         return max;
     }
 }
