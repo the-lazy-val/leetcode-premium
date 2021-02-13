f23a803b99a254fa918f0666b7adc497fb1d2137
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-02-13 19:58:44 +0530

    Update 117_Next_right_pointer_II.java

--- trees/117_Next_right_pointer_II.java
+++ trees/117_Next_right_pointer_II.java
@@ -1,4 +1,41 @@
 /**
+
+O(N) time
+O(1) space (but internal stack due to recursion is there)
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if(root==null)  return root;
+        
+        Node head = root;
+        
+        while(head!=null){
+            Node dummy = new Node(0); //this will remain at start, since we need this reference for next head
+            Node temp = dummy; //this one will move along next.. next.. next..
+            
+            while(head!=null){
+                if(head.left!=null){
+                    temp.next=head.left;
+                    temp=temp.next;
+                }
+                
+                if(head.right!=null){
+                    temp.next=head.right;
+                    temp=temp.next;
+                }
+                
+                head=head.next; //At level-1, first head=2, now head=3
+            }
+            
+            head = dummy.next; //next level
+        }
+        
+        return root;
+    }
+}
+
+/**
 BFS approach
 O(N) time
 O(N) space for queue
