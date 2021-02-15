9d0dc57c21a9e5310be542428f0e6aa8923b673f
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-02-15 22:51:38 +0530

    Create 857_min_cost_K_workers_optimized.java

---
+++ google-frequency/857_min_cost_K_workers_optimized.java
@@ -0,0 +1,61 @@
+class Solution {
+    public double mincostToHireWorkers(int[] quality, int[] wage, int K) {
+        
+        
+        double min = Double.MAX_VALUE;
+        
+        Pair<Double, Integer>[] ratioAndQuality = new Pair[wage.length];
+        
+        for(int worker=0; worker<wage.length; worker++){
+            double workerRatio = (double)wage[worker]/quality[worker];
+            ratioAndQuality[worker] = new Pair(workerRatio, quality[worker]);
+        }
+        
+        /**
+        Following the first rule: offer[x] >= wage[x]
+        => quality[x] * ratio[captain] >= wage[x]
+        => ratio[captain] >= ratio[x]
+        
+        So, only that captain can be selected later, who has atlest k-1 workers with lower ratio
+        */
+        Arrays.sort(ratioAndQuality, (a,b) -> Double.compare(a.getKey(), b.getKey()));
+        
+        // System.out.println(Arrays.toString(ratioAndQuality));
+        
+        PriorityQueue<Integer> topKpq = new PriorityQueue(Collections.reverseOrder()); //descending, as we will maintain only lowest K
+        int qualitySum=0;
+        
+        for(int worker=0; worker<K; worker++){ //0 - k-1 as they can never be captain
+            topKpq.add(ratioAndQuality[worker].getValue());
+            qualitySum+=ratioAndQuality[worker].getValue();
+        }
+        
+        double captainRatio = ratioAndQuality[K-1].getKey(); //since this is the first valid captain
+        double currCost = captainRatio * qualitySum;
+        
+        min = Math.min(min, currCost);
+        
+        //now checking other captains
+        for(int captain=K; captain<wage.length; captain++){
+            
+            captainRatio = ratioAndQuality[captain].getKey();
+            
+            int captainQuality = ratioAndQuality[captain].getValue();
+            
+            if(!topKpq.isEmpty() && captainQuality < topKpq.peek()){
+                qualitySum = qualitySum - topKpq.poll();
+                
+                topKpq.add(captainQuality);
+                
+                qualitySum += captainQuality;
+                
+                currCost = captainRatio * qualitySum;
+                
+                min = Math.min(min, currCost);
+            }
+            
+        }
+        
+        return min;
+    }
+}
