49202b23a49b2b0da4c0177e3770d4ce4f51a2ec
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-02-23 08:54:35 +0530

    Update 312_burst_ballons_backtracking_TLE.java

--- dp/312_burst_ballons_backtracking_TLE.java
+++ dp/312_burst_ballons_backtracking_TLE.java
@@ -1,3 +1,53 @@
+//Using treemap instead of visited HashSet
+
+class Solution {
+    
+    int max = -1;
+    
+    public int getSum(int[] nums, TreeSet<Integer> available, int index){
+        Integer low = available.floor(index);
+        Integer high = available.ceiling(index);
+        
+        int lowValue = (low == null) ? 1 : nums[low];
+        int highValue = (high == null) ? 1 : nums[high];
+        
+        return lowValue * nums[index] * highValue;
+    }
+    
+    public void backtrack(int[] nums, int n, TreeSet<Integer> visited, int sum){
+        if(visited.size() == 0){
+            max = Math.max(max, sum);
+            return;
+        }
+        
+        for(int i=0; i<n; i++){
+            if(visited.contains(i)){
+                visited.remove(i);
+                
+                backtrack(nums, n, visited, sum + getSum(nums, visited, i));
+                
+                visited.add(i);
+            }
+        }
+    }
+    
+    public int maxCoins(int[] nums) {
+        
+        int n = nums.length;
+        
+        TreeSet<Integer> ts = new TreeSet();
+        for(int i=0; i<n; i++){
+            ts.add(i);
+        }
+        
+        backtrack(nums, n, ts, 0);
+        
+        return max;
+    }
+}
+
+//using HasSet for visited
+
 class Solution {
     
     int max = -1;
