125741e79f93a51f7f4891e8ede175eb21ee95b7
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-02-25 11:10:02 +0530

    Create 325_max_size_subarray_with_sum_K.java

---
+++ sliding-window/325_max_size_subarray_with_sum_K.java
@@ -0,0 +1,25 @@
+class Solution {
+    //variable size sliding window
+    public int maxSubArrayLen(int[] nums, int k) {
+        HashMap<Integer, Integer> hm = new HashMap(); //sum -> index
+        
+        hm.put(0, -1); //since we have to find earliest index with given sum, for 0 its before the array
+        
+        int sum = 0;//will have sum from index 0
+        int maxLen = 0;
+        
+        for(int i=0; i<nums.length; i++){
+            sum += nums[i];
+            
+            int find = sum-k; //if there is a sum-k, which occurred before, this means from THAT index till now, sum = k
+            
+            if(hm.containsKey(find)){
+                maxLen = Math.max(maxLen, i - hm.get(find));
+            }
+            
+            hm.putIfAbsent(sum, i); //"putIfAbsent" because we only want the earliest index
+        }
+        
+        return maxLen;
+    }
+}
