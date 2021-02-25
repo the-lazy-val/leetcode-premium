1d911e58f314d2a241ecbc5a29d35278cba17202
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-02-26 00:20:36 +0530

    Update 395_longest_substring_atleast_K_repeating_chars.java

--- divide-conquer/395_longest_substring_atleast_K_repeating_chars.java
+++ divide-conquer/395_longest_substring_atleast_K_repeating_chars.java
@@ -3,14 +3,14 @@ class Solution {
     public int getLongest(char[] s, int start, int end, int k){
         if(end-start+1 < k) return 0;
         
-        int[] charCount = new int[26];
+        int[] charCount = new int[26];//get char count of all chars in this window (start -> end)
         for(int i=start; i<=end; i++){
             charCount[s[i]-'a']++;
         }
         
         for(int mid = start; mid<=end; mid++){
-            if(charCount[s[mid] - 'a'] < k){
-                return Math.max(getLongest(s, start, mid-1, k),  getLongest(s, mid+1, end, k));
+            if(charCount[s[mid] - 'a'] < k){ //if any char has count < k, then this range (start -> end) cannot be valid max
+                return Math.max(getLongest(s, start, mid-1, k),  getLongest(s, mid+1, end, k)); //so break into 2 from that index
             }
         }
         
