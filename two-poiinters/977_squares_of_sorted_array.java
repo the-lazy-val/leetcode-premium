ff1d44cfa7cd052ea71f76d42bbf04f2f4b22be6
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2021-02-26 21:50:42 +0530

    Update 977_squares_of_sorted_array.java

--- two-poiinters/977_squares_of_sorted_array.java
+++ two-poiinters/977_squares_of_sorted_array.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] sortedSquares(int[] nums) {
         
         int[] output = new int[nums.length];
-        int k = nums.length-1;
+        int k = nums.length-1;//to start filling output from the end with the bigger square number
         
         int i=0;
         int j=nums.length-1;
@@ -12,7 +12,7 @@ class Solution {
             int leftSqr = nums[i] * nums[i];
             int rightSqr = nums[j] * nums[j];
             
-            if(i==j){
+            if(i==j){ //only 1 element left now
                 output[k] = leftSqr;
                 k--;
                 i++;
